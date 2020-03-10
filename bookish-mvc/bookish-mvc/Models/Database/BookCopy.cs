1379476deb0d8531dcbf634aa68228b6ae0feb53
espe-on <eljoe0@googlemail.com>
2020-03-10 15:02:05 +0000

    defined book type and copy

---
+++ bookish-mvc/bookish-mvc/Models/Database/BookCopy.cs
@@ -0,0 +1,10 @@
+﻿namespace bookish_mvc.Models.Database
+{
+    public class BookCopy
+    {
+        public int Id { get; set; }
+        public string Isbn { get; set; }
+        public int CurrentUserId { get; set; }
+        public string DueDate { get; set; }
+    }
+}
---
+++ bookish-mvc/bookish-mvc/Models/Database/BookType.cs
@@ -0,0 +1,11 @@
+﻿namespace bookish_mvc.Models.Database
+{
+    public class BookType
+    {
+        public string Isbn { get; set; }
+        public string Title { get; set; }
+        public string AuthorFirstName { get; set; }
+        public string AuthorLastName { get; set; }
+        public int PublishYear { get; set; }
+    }
+}
