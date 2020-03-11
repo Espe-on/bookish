bf6e69820d1ffa35d72a8342335145ffdfb7b0b8
espe-on <eljoe0@googlemail.com>
2020-03-11 11:09:44 +0000

    new book checkout class

---
+++ bookish-mvc/bookish-mvc/Models/Database/BookCheckout.cs
@@ -0,0 +1,11 @@
+﻿namespace DefaultNamespace
+{
+    public class BookCheckout
+    {
+        public int Id { get; set; }
+        public string takenDate { get; set; }
+        public string returnedDate { get; set; }
+        public int bookCopyId { get; set; }
+        public int userId { get; set; }
+    }
+}
