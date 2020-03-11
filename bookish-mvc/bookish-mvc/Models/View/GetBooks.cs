bb2da389a36c1d881a73b7f26836c95d911afa5c
espe-on <eljoe0@googlemail.com>
2020-03-11 12:55:46 +0000

    new class for getting books

---
+++ bookish-mvc/bookish-mvc/Models/View/GetBooks.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+using bookish_mvc.Models.Database;
+
+namespace bookish_mvc.Models.View
+{
+    public class GetBooks
+    {
+        public List<BookType> BookList { get; set; }
+    }
+}
