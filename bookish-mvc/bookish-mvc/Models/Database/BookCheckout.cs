30cf23115a1f2cb14106c5dcf0d9e600d705a824
espe-on <eljoe0@googlemail.com>
2020-03-11 12:55:33 +0000

    Syntax updates

--- bookish-mvc/bookish-mvc/Models/Database/BookCheckout.cs
+++ bookish-mvc/bookish-mvc/Models/Database/BookCheckout.cs
@@ -1,11 +1,11 @@
-﻿namespace DefaultNamespace
+﻿namespace bookish_mvc.Models.Database
 {
     public class BookCheckout
     {
         public int Id { get; set; }
-        public string takenDate { get; set; }
-        public string returnedDate { get; set; }
-        public int bookCopyId { get; set; }
-        public int userId { get; set; }
+        public string TakenDate { get; set; }
+        public string ReturnedDate { get; set; }
+        public int BookCopyId { get; set; }
+        public int UserId { get; set; }
     }
 }
