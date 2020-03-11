d650f02d3038d8b75f7134840e34ad7dea3ee898
espe-on <eljoe0@googlemail.com>
2020-03-11 12:55:06 +0000

    new controller for doing stuff with books

---
+++ bookish-mvc/bookish-mvc/Controllers/BooksController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using bookish_mvc.Models.Database;
+using bookish_mvc.Models.View;
+using Microsoft.AspNetCore.Mvc;
+
+namespace bookish_mvc.Controllers
+{
+    public class BooksController : Controller
+    {
+        public IActionResult GetBooks()
+        {
+            // var bookList = myBookService.getAllBooks();
+            
+            var getBooksModel = new GetBooks
+            {
+                // BookList = bookList
+            };
+            return View(getBooksModel);
+        }
+    }
+}
