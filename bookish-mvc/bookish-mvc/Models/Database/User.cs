21571f0baafde46e96da0730d65faf601ec7474d
espe-on <eljoe0@googlemail.com>
2020-03-11 12:54:35 +0000

    Syntax update

--- bookish-mvc/bookish-mvc/Models/Database/User.cs
+++ bookish-mvc/bookish-mvc/Models/Database/User.cs
@@ -5,9 +5,6 @@ namespace bookish_mvc.Models.Database
 {
     public class User
     {
-        [Key]
-        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
-
         public int Id { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
