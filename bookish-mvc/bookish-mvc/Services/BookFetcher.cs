ce98017873630ada5adcc69d227a82d7b5ecd499
espe-on <eljoe0@googlemail.com>
2020-03-13 13:59:10 +0000

    Created Book Fetcher

---
+++ bookish-mvc/bookish-mvc/Services/BookFetcher.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using bookish_mvc.Models.Database;
+using Dapper;
+using MySql.Data.MySqlClient;
+
+namespace bookish_mvc.Services
+{
+    public class BookFetcher
+    {
+        public static void bookFetcher()
+        {
+            using var connection = new MySqlConnection(
+                $"Server={Environment.GetEnvironmentVariable("DBURL")};" +
+                "Database=bookish;" +
+                "Uid=root;" +
+                $"Pwd={Environment.GetEnvironmentVariable("DBPASSWORD")}");
+            IEnumerable<BookType> allTheBooks = connection.Query<BookType>("SELECT * from book_type");
+        }
+    }
+}
